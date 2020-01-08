746285e4675d7fe958f7dc634654d5ffb24ea3a7
rai <richard.iserman@gmail.com>
2020-01-07 22:49:31 -0500

    making some progress in implementing user management using passport-local-sequelize package. model is defined and called in app.js here, just need to define routes and views now.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require ('passport');
+var User = require('./models/user');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var playRouter = require('./routes/play'); // Import routes for "play" area of site
 
 var app = express();
 
@@ -16,9 +19,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session') ({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(require('connect-multiparty')());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/play', playRouter);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 module.exports = app;
