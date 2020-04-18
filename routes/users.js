429ded167664a09ffc43578c3775003db7b2813e
rai <richard.iserman@gmail.com>
2020-04-18 17:56:08 -0400

    may revert to jan 11 version and just try to work from that

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 
-var models = require('../models');
+var User = require('../models0/user');
 
 router.get('/login', function(req, res) {
   res.render('login', { user: req.user });
@@ -22,8 +22,9 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register', function(req, res) {
-  models.User.register(new models.User({ username : req.body.username }), req.body.password, function(err, user) {
+  User.register(new User({ username : req.body.username }), req.body.password, req.body.email, function(err, user) {
     if (err) {
+      console.log(err);
       return res.render('register', { user : user });
     }
 
