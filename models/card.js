886aa00aa10c282693f1613da4c0629b869fbc5b
rai <richard.iserman@gmail.com>
2022-01-28 16:44:22 -0500

    restoring card model

--- models/card.js
+++ models/card.js
@@ -1,27 +1,19 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Card extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Card.init({
-    id: DataTypes.INTEGER,
+  const Card = sequelize.define('Card', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     phone: DataTypes.STRING,
     hashedId: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Card',
-  });
+  }, {});
+  Card.associate = function(models) {
+    Card.belongsTo(models.Game);
+  };
   return Card;
 };
