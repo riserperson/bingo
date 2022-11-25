0ed5eddd53801fe7e918a2c58f71e818e2dbff7d
rai <richard.iserman@gmail.com>
2022-11-24 20:53:29 -0500

    removing passport

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var passport = require ('passport');
-
+const dotenv = require('dotenv').config()
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 var playRouter = require('./routes/play'); // Import routes for "play" area of site
 
 var models = require('./models');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +26,6 @@ app.use(require('express-session') ({
   saveUninitialized: false
 }));
 app.use(require('connect-multiparty')());
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
