444ce5153d8415057b0c801ebe3148a3911bb0b5
rai <richard.iserman@gmail.com>
2022-12-29 13:42:56 -0500

    fixing

--- migrations/20220131011538-add-association-columns.js
+++ migrations/20220131011538-add-association-columns.js
@@ -2,66 +2,64 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    if (process.env.DATABASE_URL == 'development') {
-      return queryInterface.addColumn(
-        'Spaces',
-        'GameId',
-        {
-          type: Sequelize.INTEGER,
-          references: {
-            model: 'Games',
-            key: 'id',
-          },
-          onUpdate: 'CASCADE',
-          onDelete: 'SET NULL',
-        }
-      )
-        .then(() => {
-          return queryInterface.addColumn(
-            'Cards',
-            'GameId',
-            {
-              type: Sequelize.INTEGER,
-              references: {
-                model: 'Games',
-                key: 'id',
-              },
-              onUpdate: 'CASCADE',
-              onDelete: 'SET NULL',
-            }
-          );
-        })
-        .then(() => {
-          return queryInterface.addColumn(
-            'Card_Spaces',
-            'CardId',
-            {
-              type: Sequelize.INTEGER,
-              references: {
-                model: 'Cards',
-                key: 'id',
-              },
-              onUpdate: 'CASCADE',
-              onDelete: 'SET NULL',
-            }
-          );
-        })
-        .then(() => {
-          return queryInterface.addColumn(
-            'Card_Spaces',
-            'SpaceId',
-            {
-              type: Sequelize.INTEGER,
-              references: {
-                model: 'Spaces',
-                key: 'id',
-              },
-              onUpdate: 'CASCADE',
-              onDelete: 'SET NULL',
-            }
-          );
-        });
-    }
+    return queryInterface.addColumn(
+      'Spaces',
+      'GameId',
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Games',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }
+    )
+      .then(() => {
+        return queryInterface.addColumn(
+          'Cards',
+          'GameId',
+          {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'Games',
+              key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+          }
+        );
+      })
+      .then(() => {
+        return queryInterface.addColumn(
+          'Card_Spaces',
+          'CardId',
+          {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'Cards',
+              key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+          }
+        );
+      })
+      .then(() => {
+        return queryInterface.addColumn(
+          'Card_Spaces',
+          'SpaceId',
+          {
+            type: Sequelize.INTEGER,
+            references: {
+              model: 'Spaces',
+              key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+          }
+        );
+      });
   },
   down: async (queryInterface, Sequelize) => {
     return queryInterface.removeColumn(
